07711d512e065f85d84a9e5eceff26ce14adf378
Gerardo Chacón Álvarez <155681027+gerardoChacon@users.noreply.github.com>
2025-09-05 21:33:41 -0600

    Agregando funcionalidad final a desplegarReglamento

--- Actividad_2/app.js
+++ Actividad_2/app.js
@@ -1,7 +1,7 @@
 class InicioPAM {
     desplegarReglamentoPAM(){
         const contenedor = document.getElementById("contenido");
-        contenedor.innerHTML = " <h1>Reglamento</h1>";
+        contenedor.innerHTML = "  <h1>Reglamento</h1><ol><li>Participación activa en clase</li><li>Trabajos en classroom </li><li>Entregas completas</li><li>Respetar tiempos de entrega </li><li>Presentación de trabajo calidad universitaria</li></ol>";
     }
 
     desplegarLineamientosClassroom(){
