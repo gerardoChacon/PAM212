7521b1d19ce564c7c37d057d895ca5a934a949e8
Gerardo Chacón Álvarez <155681027+gerardoChacon@users.noreply.github.com>
2025-09-05 21:43:15 -0600

    Agregando funcionalidad final a todas las funciones restantes

--- Actividad_2/app.js
+++ Actividad_2/app.js
@@ -1,18 +1,22 @@
 class InicioPAM {
     desplegarReglamentoPAM(){
+        //la variable contenedor consigue en el DOM al div con id "contenido" y innerHTML lo modifica.
         const contenedor = document.getElementById("contenido");
-        contenedor.innerHTML = "  <h1>Reglamento</h1><ol><li>Participación activa en clase</li><li>Trabajos en classroom </li><li>Entregas completas</li><li>Respetar tiempos de entrega </li><li>Presentación de trabajo calidad universitaria</li></ol>";
+        contenedor.innerHTML = " <h1>Reglamento</h1><ol><li>Participación activa en clase</li><li>Trabajos en classroom </li><li>Entregas completas</li><li>Respetar tiempos de entrega </li><li>Presentación de trabajo calidad universitaria</li></ol>";
     }
 
     desplegarLineamientosClassroom(){
-        console.log("Lineamiento");
+        const contenedor = document.getElementById("contenido");
+        contenedor.innerHTML = "<h1>Lineaminetos</h1><ol><li>Todos los trabajos llevan portada estilo libre: Logo UPQ, tema, datos de alumno, materia</li><li>Conclusiones de aprendizaje: Descripción de lo aprendido durante el desarrollo de la actividad </li></ol>";
     }
 
     desplegarFechasDeParciales(){
-        console.log("Fechas");
+        const contenedor = document.getElementById("contenido");
+        contenedor.innerHTML = " <h1>Fechas de Exmámenes Parciales</h1><ul><li>Parcial 1: 29/09/2025</li><li>Parcial 2: 03/10/2025</li><li>Parcial 3: 01/12/2025</li></ul>";
     }
 
     desplegarPorcentajesPorParcial(){
-        console.log("Porcentajes");
+        const contenedor = document.getElementById("contenido");
+        contenedor.innerHTML = "<h1>Porcentajes</h1><ul><li>Parcial 1<ol><li>Conocimiento: 40%</li><li>Desempeño: 20%</li><li>Productyo: 30%</li><li>PI: 10%</li></ol></li><li>Parcial 2<ol><li>Conocimiento: 40%</li><li>Desempeño: 20%</li><li>Productyo: 20%</li><li>PI: 20%</li></ol></li><li>Parcial 3<ol><li>Conocimiento: 20%</li><li>Desempeño: 10%</li><li>Productyo: 40%</li><li>PI: 30%</li></ol></li></ul>";
     }
 }
